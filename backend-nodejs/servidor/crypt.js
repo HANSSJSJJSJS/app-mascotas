341a03b8a5acf36fcf8ddc9234bfa3de815a929f
22092024KJ <castilloana2329@gmail.com>
2025-06-20 16:47:13 -0500

    Cambios en citas y en historial

--- backend-nodejs/servidor/crypt.js
+++ backend-nodejs/servidor/crypt.js
@@ -5,8 +5,8 @@ async function encryptPasswords() {
     const db = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: '12345678',
-        port: 3309,
+        password: '',
+        port: 3306,
         database: 'mascotas_db'
     });
 
